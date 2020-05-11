2668f24b6771c4e1e5c817379f50859f46865f37
Sean Glancy <spglancy@gmail.com>
2020-05-11 10:05:01 -0700

    Updates

---
+++ precache-manifest.3bfbd2d4c6b421b0debd476704710f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06dcff3b89b189b0c102",
+    "url": "/tetris-clone/static/js/runtime~main.cc0a72ef.js"
+  },
+  {
+    "revision": "f7e33816b865546cedfd",
+    "url": "/tetris-clone/static/js/main.ce1a261c.chunk.js"
+  },
+  {
+    "revision": "57668a8daa27cbd7b4ea",
+    "url": "/tetris-clone/static/js/2.d2a2e951.chunk.js"
+  },
+  {
+    "revision": "f7e33816b865546cedfd",
+    "url": "/tetris-clone/static/css/main.8a038ab4.chunk.css"
+  },
+  {
+    "revision": "babca86a796e4e0fc066e77f6a899e83",
+    "url": "/tetris-clone/index.html"
+  }
+];
